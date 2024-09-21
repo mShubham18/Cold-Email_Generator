2af1d1327e8a4c23286654d47f9c547632ce1873
Shubham <shubhuu1804@gmail.com>
2024-09-21 20:44:58 +0530

    Updated Portfolio.py

--- app/portfolio.py
+++ app/portfolio.py
@@ -6,7 +6,9 @@ class Portfolio:
     def __init__(self, file_path = "app/Resource/my_portfolio.csv"):
         self.file_path = file_path
         self.data = pd.read_csv(file_path)
-        self.client = chromadb.PersistentClient("vectorstore")
+        #self.client = chromadb.PersistentClient("vectorstore")
+        self.client = chromadb.Client(settings={"persist_directory": "vectorstore"})
+        #
         self.collection = self.client.get_or_create_collection(name="portfolio")
 
     def load_portfolio(self):
